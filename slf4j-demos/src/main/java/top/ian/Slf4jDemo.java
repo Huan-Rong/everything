c9e588fd86680b05e7ad4c629c58d6b7021aceab
Huan-Rong <zhenghuanrong@yeah.net>
2017-05-10 19:09:28 +0800

    Add notes about SLF4J and Logback.

--- slf4j-demos/src/main/java/top/ian/Slf4jDemo.java
+++ slf4j-demos/src/main/java/top/ian/Slf4jDemo.java
@@ -1,9 +1,14 @@
 package top.ian;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 需要注意的是，这个例子并没有引用任何的 logback 类，而是 import SLF4J 的类文件。
+ *
+ * 在绝大多数情况下，我们只需要 import SLF4J，并且不会去在意 logback 的存在。
  * Created by ian on 5/7/17.
  */
 public class Slf4jDemo {
@@ -11,5 +16,14 @@ public class Slf4jDemo {
 
         Logger logger = LoggerFactory.getLogger(Slf4jDemo.class);
         logger.info("Hello World!");
+        try {
+           int i = 1/0;
+        } catch (Exception e) {
+            logger.error("Test", e);
+        }
+
+        // print internal state
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
     }
 }
